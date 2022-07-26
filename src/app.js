824b2f7a2740a22d177a7ec90230d785be7dfa6d
Padilla2012 <padilla-silva@hotmail.com>
2022-07-25 20:28:08 -0500

    working on feature questions

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+
+/*const fs = require('fs');
+
 const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2);
@@ -15,4 +18,14 @@ fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
-});
+});*/
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
